0fd991eb9a0d2d1e0047506044bd8afd7f4594c1
slavs <a@ldd.so>
2023-08-12 08:14:29 +0000

    fix separators

--- MainWindow.h
+++ MainWindow.h
@@ -9,53 +9,65 @@
 #include "FolderModelColumns.h"
 
 class MainWindow : public Gtk::Window {
-public:
-    MainWindow(Gtk::Window::BaseObjectType* win, const Glib::RefPtr<Gtk::Builder>& builder);
+    public:
+        MainWindow(Gtk::Window::BaseObjectType* win, const Glib::RefPtr<Gtk::Builder>& builder);
 
-private:
+    private:
 
-    const Glib::RefPtr<Gtk::Builder> m_builder;
-    Gtk::ToolButton* m_upload_button;
-    Gtk::ToolButton* m_create_folder;
-    Gtk::ToolButton* m_open_archive_button;
-    Gtk::ToolButton* m_new_archive_button;
-    Gtk::ToolButton* m_archive_settings_button;
-    Gtk::ToolButton* m_add_new_media_button;
-    Gtk::TreeView* m_tree;
-    Gtk::TreeSelection* m_selection;
-    Gtk::TreeStore* m_tree_store;
-    Gtk::TreeView* m_contents_view;
-    Gtk::ListStore* m_contents_store;
-    Gtk::TreeView* m_media_view;
-    Gtk::ListStore* m_media_store;
-    std::unordered_map<uint64_t, Gtk::TreeIter> m_tree_fast_access;
+        const Glib::RefPtr<Gtk::Builder> m_builder;
+        Gtk::ToolButton* m_upload_button;
+        Gtk::ToolButton* m_create_folder;
+        Gtk::ToolButton* m_open_archive_button;
+        Gtk::ToolButton* m_new_archive_button;
+        Gtk::ToolButton* m_archive_settings_button;
+        Gtk::ToolButton* m_add_new_media_button;
+        Gtk::TreeView* m_tree;
+        Gtk::TreeSelection* m_selection;
+        Gtk::TreeStore* m_tree_store;
+        Gtk::TreeView* m_contents_view;
+        Gtk::ListStore* m_contents_store;
+        Gtk::TreeView* m_media_view;
+        Gtk::ListStore* m_media_store;
+        Gtk::SeparatorToolItem* m_sep1;
+        Gtk::SeparatorToolItem* m_sep2;
+        std::unordered_map<uint64_t, Gtk::TreeIter> m_tree_fast_access;
 
-    void onNewArchiveButtonClicked();
-    void onOpenArchiveButtonClicked();
-    void onArchiveSettings();
-    void onNewMediaButtonClicked();
-    void onCreateFolderClicked();
-    void updateUI();
-    void updateTree();
-    void updateContents();
-    void updateMediaView();
-    void onUploadButtonClicked();
-    void onMediaToggle(const Glib::ustring& path);
+        void onNewArchiveButtonClicked();
 
-    uint64_t currentFolderParentId();
+        void onOpenArchiveButtonClicked();
 
-    template <typename A, typename B, typename C>
-    void allocateTreeNode(A it, B id, C name) {
-        m_tree_fast_access[id] = it;
-        const auto& row = *it;
+        void onArchiveSettings();
 
-        FolderModelColumns cols;
-        row[cols.folder] = name;
-        row[cols.id] = id;
-        row[cols.status] = Gdk::Pixbuf::create_from_resource(id == 1 ? "/icons/ca-root.svg" : "/icons/ca-folder.svg");
-    }
+        void onNewMediaButtonClicked();
 
-    void allocateTreeNodeUsingParentId(const Glib::ustring &folderName, uint64_t id, uint64_t parentId);
+        void onCreateFolderClicked();
+
+        void updateUI();
+
+        void updateTree();
+
+        void updateContents();
+
+        void updateMediaView();
+
+        void onUploadButtonClicked();
+
+        void onMediaToggle(const Glib::ustring& path);
+
+        uint64_t currentFolderParentId();
+
+        template<typename A, typename B, typename C>
+        void allocateTreeNode(A it, B id, C name) {
+            m_tree_fast_access[id] = it;
+            const auto& row = *it;
+
+            FolderModelColumns cols;
+            row[cols.folder] = name;
+            row[cols.id] = id;
+            row[cols.status] = Gdk::Pixbuf::create_from_resource(id == 1 ? "/icons/ca-root.svg" : "/icons/ca-folder.svg");
+        }
+
+        void allocateTreeNodeUsingParentId(const Glib::ustring& folderName, uint64_t id, uint64_t parentId);
 
         std::string collectExclusions();
 };
